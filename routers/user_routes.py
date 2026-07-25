f6cfa1a006102569ce3f79d7163bf69fa2a34fa2
Kushagra Saini <kushagra.saini19@gmail.com>
2026-07-25 23:46:01 +0530

    shifted all logic to service layer

--- routers/user_routes.py
+++ routers/user_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from schema.userDTO import Create_user, User_response, AdminDashboardResponse
 from database.database import get_db
 from database.models import User
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Session, joinedload, selectinload, load_only
 from utils.security import hash_password
 from dependencies.permissions import admin_required
 import services.user_services as user_services
+from dependencies.auth import get_current_user
 
 user_router = APIRouter(
     prefix = "/user",
@@ -22,19 +23,7 @@ def register_user(payload : Create_user, db : Session = Depends(get_db)):
 
 @user_router.get("/")
 def get_all_users(db : Session = Depends(get_db), current_user = Depends(admin_required)):
-
-    all_users = db.query(User).all()
-
-    return {
-        "Total" : len(all_users),
-        "users" :
-          [{
-            "id" : user.id,
-            "name" : user.name,
-            "email" : user.email
-            }
-        for user in all_users]
-    }
+    return user_services.get_all_users(db=db)
 
 @user_router.get("/get_user_by_id/{user_id}", response_model = User_response)
 def get_user_by_id(user_id : int ,db: Session = Depends(get_db)):
@@ -46,34 +35,22 @@ def get_user_by_id(user_id : int ,db: Session = Depends(get_db)):
 # USER PROFILE -->
 @user_router.get("/users/{user_id}")
 def user_profile(user_id : int, db : Session = Depends(get_db)):
-    user = (
-        db.query(User).options(selectinload(User.posts)).filter(User.id == user_id).first()
+    return user_services.user_profile(
+        db,
+        user_id
     )
-
-    if not user:
-        raise HTTPException(
-            status_code = 404,
-            detail = "User not found"
-        )
-
-    return {
-        "id" : user.id,
-        "name" : user.name,
-        "email" : user.email,
-        "posts" : [
-            {
-                "post_id" : post.id,
-                "title" : post.title,
-                "description" : post.description
-            }
-            for post in user.posts
-        ]
-    }
 
 # ADMIN DASHBOARD -->
 @user_router.get("/admin_dashboard", response_model = list[AdminDashboardResponse])
-def admin_dashboard(db : Session = Depends(get_db)):
-    users = (
-        db.query(User).options(load_only(User.id, User.name),selectinload(User.posts)).order_by(User.id).offset(5).limit(6).all()
+def admin_dashboard(
+    db : Session = Depends(get_db),
+    limit : int = Query(10, ge = 0, le = 100),
+    offset : int = Query(0),
+    current_user = Depends(get_current_user)):
+
+    return user_services.admin_dashboard(
+        db,
+        limit,
+        offset,
+        current_user
     )
-    return users
--- services/user_services.py
+++ services/user_services.py
@@ -3,6 +3,7 @@ from utils.security import hash_password
 from database.models import User
 from fastapi import  HTTPException
 from sqlalchemy.orm import Session, joinedload, selectinload, load_only
+from dependencies.permissions import admin_required
 
 # USER REGISTER -->
 def register_user(db : Session, payload : Create_user):
@@ -18,7 +19,7 @@ def register_user(db : Session, payload : Create_user):
         name = payload.name,
         email = payload.email,
         password = hash_password(payload.password),
-        role = "user"
+        role = "admin"
     )
 
     db.add(user)
@@ -39,3 +40,63 @@ def get_user_by_id(db : Session, user_id : int):
 
     return user
 
+# GET ALL USERS -->
+def get_all_users(db : Session):
+    users = db.query(User).all()
+
+    return {
+        "total_users" : len(users),
+        "users" : [
+            {
+                'user_id' : id,
+                'name' : name,
+                'email' : email
+            }
+            for id, name, email in users
+        ]
+    }
+
+# USER PROFILE -->
+def user_profile(db : Session, user_id : int):
+    user = (
+        db.query(User).options(selectinload(User.posts)).filter(User.id == user_id).first()
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code = 404,
+            detail = "User not found"
+        )
+
+    return {
+        "id" : user_id,
+        "name" : user.name,
+        "email" : user.email,
+        "posts" : [
+            {
+                "id" : post.id,
+                "title" : post.title,
+                "description" : post.description
+            }
+            for post in user.posts
+        ]
+    }
+
+# ADMIN DASHBOARD -->
+def admin_dashboard(db : Session, limit : int , offset : int,current_user : User):
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code = 403,
+            detail = "Unauthorized"
+        )
+
+    users = (
+        db.query(User)
+        .options(load_only(User.id, User.name),selectinload(User.posts))
+        .order_by(User.id)
+        .limit(limit)
+        .offset(offset)
+        .all()
+    )
+
+    return users

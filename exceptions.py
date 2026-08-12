5359bf52e06f7d8091c98c1052d05a4aefae3993
Kushagra Saini <kushagra.saini19@gmail.com>
2026-08-12 23:58:16 +0530

    converted into async endpoints

--- exceptions.py
+++ exceptions.py
@@ -1,6 +1,7 @@
 from fastapi import HTTPException, Request
 from fastapi.responses import JSONResponse
 from app.main import app
+from app.exceptions.custom import UserNotFoundError
 
 @app.exception_handler(ValueError)
 async def value_error_handler(
@@ -9,6 +10,15 @@ async def value_error_handler(
 ):
     return JSONResponse(
         status_code = 400,
+        content = {
+            "message" : str(exc)
+        }
+    )
+
+@app.exception_handler(UserNotFoundError)
+async def user_not_found_handler(request : Request, exc : UserNotFoundError):
+    return JSONResponse(
+        status_code = 404,
         content = {
             "message" : str(exc)
         }
